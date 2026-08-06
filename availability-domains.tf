fc5deb7828dc95415e14cafd167a41422a56f49e
Vasilis <vsiaploulis@blendtechnologies.com>
2026-08-06 12:08:06 +0300

    Track availability domain data source

---
+++ availability-domains.tf
@@ -0,0 +1,13 @@
+# Source from https://registry.terraform.io/providers/oracle/oci/latest/docs/data-sources/identity_availability_domains
+
+# Tenancy is the root or parent to all compartments.
+# For this tutorial, use the value of <tenancy-ocid> for the compartment OCID.
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_id
+}
+
+# Then pick the first AD dynamically:
+locals {
+  first_ad = data.oci_identity_availability_domains.ads.availability_domains[0].name
+}
